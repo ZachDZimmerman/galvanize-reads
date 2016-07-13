279ec7caf5664dbfe70ff70912bddb5b5b6fb969
ZachZimmerman7 <zzimmerman@gmail.com>
2016-07-13 10:10:55 -0600

    add book functionality working

--- db/api.js
+++ db/api.js
@@ -1,6 +1,37 @@
 var knex = require('./knex');
 module.exports = {
-  listBook: function() {
+  insertGenre: function (genre) {
+    return knex('genre').insert({
+      name: genre
+    }, 'id');
+  },
+  listBook: function (id) {
+    return knex('book').where('id', id).first();
+  },
+  listAllBooks: function() {
     return knex('book').select();
+  },
+  getGenre: function () {
+    return knex('genre').select();
+  },
+  addBook: function (data) {
+    if (data.genre_id[1]) {
+      return this.insertGenre(data.genre_id[1])
+      .then(function (id) {
+        return knex('book').insert({
+          title: data.title,
+          genre_id: id[1],
+          description: data.description,
+          cover_url: data.cover_url
+        });
+      });
+    } else {
+      return knex('book').insert({
+        title: data.title,
+        genre_id: data.genre_id[0],
+        description: data.description,
+        cover_url: data.cover_url
+      });
+    }
   }
-}
+};
--- routes/book.js
+++ routes/book.js
@@ -3,10 +3,21 @@ var router = express.Router();
 var api = require('../db/api');
 
 /* GET book listing. */
-router.get('/', function(req, res) {
-  var book = api.listBook().then(function (book) {
+router.get('/', function(req, res, next) {
+  var book = api.listAllBooks().then(function (book) {
     res.render("book", {book:book});
   });
 });
 
+router.get('/new', function (req, res, next) {
+  var genre = api.getGenre().then(function (genre) {
+    res.render('add-book', {genre:genre});
+  });
+});
+
+router.post('/', function (req, res, next) {
+    api.addBook(req.body).then(function () {
+      res.redirect('/book');
+    });
+});
 module.exports = router;
