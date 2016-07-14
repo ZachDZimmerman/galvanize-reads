16b262c05f89a46750f1b703efd17df1c7351919
ZachZimmerman7 <zzimmerman@gmail.com>
2016-07-13 20:47:47 -0600

    Too many changes, dont do that again

--- db/api.js
+++ db/api.js
@@ -1,40 +1,32 @@
 var knex = require('./knex');
+
+
 module.exports = {
-  insertGenre: function (genre) {
-    return knex('genre').insert({
-      name: genre
-    }, 'id');
-  },
-  listBook: function (id) {
-    return knex('book').where('id', id).first();
+  listBookById: function (id) {
+    return knex('book')
+            .join('genre', 'book.genre_id', '=', 'genre.id')
+            .select('book.id as id', 'book.title', 'genre.name as genre', 'book.description', 'book.cover_url')
+            .where({'book.id': id})
+            .first();
   },
   listAllBooks: function() {
-    return knex('book').select();
+    return knex('book')
+            .join('genre', 'book.genre_id', '=', 'genre.id')
+            .select('book.id as id', 'book.title', 'genre.name as genre', 'book.description', 'book.cover_url');
+            // .orderBy('book.title', 'asc');
   },
   getGenre: function () {
     return knex('genre').select();
+    // .orderBy('name', 'asc');
+
   },
   deleteBook: function (id) {
     return knex('book').where('id', id).del();
   },
-  addBook: function (data) {
-    if (data.genre_id[1]) {
-      return this.insertGenre(data.genre_id[1])
-      .then(function (id) {
-        return knex('book').insert({
-          title: data.title,
-          genre_id: id[1],
-          description: data.description,
-          cover_url: data.cover_url
-        });
-      });
-    } else {
-      return knex('book').insert({
-        title: data.title,
-        genre_id: data.genre_id[0],
-        description: data.description,
-        cover_url: data.cover_url
-      });
-    }
+  editBook: function (book, bookId) {
+    return knex('book').where({id:bookId}).update(book);
+  },
+  addBook: function (book) {
+    return knex('book').insert(book).returning('id');
   }
 };
--- routes/book.js
+++ routes/book.js
@@ -2,34 +2,66 @@ var express = require('express');
 var router = express.Router();
 var api = require('../db/api');
 
-/* GET book listing. */
+// READ BOOK
 router.get('/', function(req, res, next) {
   var book = api.listAllBooks().then(function (book) {
-    res.render("book/book", {book:book});
+    res.render('book/book', {book:book});
   });
 });
 
+
+// ADD BOOK
 router.get('/new', function (req, res, next) {
   var genre = api.getGenre().then(function (genre) {
     res.render('book/add-book', {genre:genre});
   });
 });
 
-router.post('/', function (req, res, next) {
-    api.addBook(req.body).then(function () {
-      res.redirect('/');
+router.post('/new', function (req, res, next) {
+  api.addBook(req.body).then(function () {
+    api.addBook().then(function () {
+      res.redirect('/book');
     });
+  });
 });
 
+// BOOK DETAILS
+
+router.get('/:id' , function (req, res, next) {
+  api.listBookById(req.params.id).then(function (book) {
+    res.render('book/detail', {book: book});
+  });
+});
+
+
+// UPDATE BOOK
+router.get('/edit/:id', function (req, res, next) {
+  api.listBookById(req.params.id).then(function (book) {
+    api.getGenre().then(function (genre) {
+      res.render('book/edit-book', {
+        book: book,
+        genre: genre
+      });
+    });
+  });
+});
+
+router.post('/edit/:id', function (req, res) {
+  api.editBook(req.body, req.params.id).then(function () {
+    res.redirect('/');
+  });
+});
+
+// DELETE BOOK
 router.get('/delete/:id', function (req, res, next) {
-  api.listBook(req.params.id).then(function (book) {
+  api.listBookById(req.params.id).then(function (book) {
     res.render('book/delete-book', {book:book});
   });
 });
 
 router.delete('/:id', function (req, res, next) {
   api.deleteBook(req.params.id).then(function () {
-    res.redirect('/');
+    res.redirect('/book');
   });
 });
 
